c948a438d53800defa3ca597d05ca782e964c3ae
tytydraco <tytydraco@users.noreply.github.com>
2019-06-21 03:05:05 +0200

    block: introduce Anxiety I/O scheduler

--- franken.sh
+++ franken.sh
@@ -21,7 +21,7 @@ export LD_LIBRARY_PATH=${HOME}/android/clangop/clang-r353983c/lib64:$LD_LIBRARY_
 DEFCONFIG="franken_defconfig"
 
 # Kernel Details
-VER=".rc2.1k"
+VER=".r2"
 
 # Paths
 KERNEL_DIR=`pwd`
