95e8d0f95abf0c67814788d051c31eccc0bbe601
Dabug123 <clarkbrunswick55@gmail.com>
2019-07-26 14:43:07 +0200

    update

--- franken.sh
+++ franken.sh
@@ -21,7 +21,7 @@ export LD_LIBRARY_PATH=${HOME}/android/clangop/clang-r353983e/lib64:$LD_LIBRARY_
 DEFCONFIG="franken_defconfig"
 
 # Kernel Details
-VER=".R9.6.9"
+VER=".R9.7"
 
 # Paths
 KERNEL_DIR=`pwd`
